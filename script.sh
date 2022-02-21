b48d9f4962fd777c50496c7dc6232dce658db1b1
SuspectWorkers <99353517+SuspectWorkers@users.noreply.github.com>
2022-02-21 11:05:45 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,15 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+
+
+while read d || [[ -n $d ]]; do
+  ip=$(curl -v --silent $host 2>&1 | grep HTTP/1.1)
+  ipu=$(curl -v --silent $host 2>&1 | grep Server:)
+  if [ -n "$ip" ]; then
+    echo "[+] '$d' => $ip    $ipu"
+    echo "$ip   $d   $ipu" >> hosts_filtered.txt
+  else
+    echo "[!] '$d' => [RESOLVE ERROR]"
+  fi
+done < hosts.txt
+
