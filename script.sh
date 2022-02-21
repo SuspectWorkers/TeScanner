21aad320839fb614cbfacc0e3f7ce7292f6db596
SuspectWorkers <99353517+SuspectWorkers@users.noreply.github.com>
2022-02-21 11:28:06 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,10 +4,10 @@ echo "" > hosts_filtered.txt
 while read d || [[ -n $d ]]; do
   ip=$(curl -s -w "%{http_code}\n" -o /dev/null $d)
   ipu=$(curl -v --silent $d 2>&1 | grep Server:)
-  ipa=$(curl -v -L --silent $d 2>&1 | grep HTTP/)
+  ipas=$(curl -L -v --silent $d 2>&1 | grep "200 OK")
   if [ -n "$ip" ]; then
     echo "[+] '$d' => $ip    $ipu"
-    echo "$ip  $ipa   $d   $ipu" >> hosts_filtered.txt
+    echo "$ip  $ipas   $d   $ipu" >> hosts_filtered.txt
   else
     echo "[!] '$d' => [RESOLVE ERROR]"
   fi
