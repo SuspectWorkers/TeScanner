34872a6980185ee061ceaba84bfd9f582d8108f9
SuspectWorkers <99353517+SuspectWorkers@users.noreply.github.com>
2022-02-21 12:17:02 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -2,13 +2,21 @@
 
 echo "" > hosts_filtered.txt
 while read d || [[ -n $d ]]; do
-  ip=$(curl -s -w "%{http_code}\n" -o /dev/null $d)
-  ipu=$(curl -v --silent $d 2>&1 | grep Server:)
-  ipas=$(curl -L -v --silent $d 2>&1 | grep "200 OK")
+  ip=$(curl -s -w "%{http_code}\n" -o /dev/null $d|head -1)
+  ipu=$(curl -v --silent $d 2>&1 | grep Server: | cut --delimiter=" " -f 3 | cut --delimiter="^" -f 1 |head -1)
+  ipas=$(curl -L -v --silent $d 2>&1 | grep "200 OK" | cut --delimiter=" " -f 2,3,4|head -1)
   if [ -n "$ip" ]; then
     echo "[+] '$d' => $ip    $ipu"
-    echo "$ip  $ipas   $d   $ipu" >> hosts_filtered.txt
+    echo "$ip	$d	$ipu	$ipas" >> hosts_filtered.txt
   else
     echo "[!] '$d' => [RESOLVE ERROR]"
   fi
 done < hosts.txt
+tr -d '\15\32' < hosts_filtered.txt > output.txt
+rm hosts_filtered.txt
+
+read -p "Do you wish to save hosts onto your main storage (y / n)? " yn
+    if [[ "$yn" =~ 'y' ]]; then continue; fi
+    if [[ "$yn" =~ 'n' ]]; then exit; fi
+
+cp output.txt /sdcard/output.txt
