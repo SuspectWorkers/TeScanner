760d713cf11f3217aadc649e471a636130ffc88f
SuspectWorkers <99353517+SuspectWorkers@users.noreply.github.com>
2022-02-21 11:12:20 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,13 +1,13 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-echo "1\n" > hosts_filtered.txt
-
+echo "" > hosts_filtered.txt
 while read d || [[ -n $d ]]; do
-  ip=$(curl -v --silent $host 2>&1 | grep HTTP/1.1)
-  ipu=$(curl -v --silent $host 2>&1 | grep Server:)
+  ip=$(curl -v --silent $d 2>&1 | grep HTTP/1.1)
+  ipu=$(curl -v --silent $d 2>&1 | grep Server:)
   if [ -n "$ip" ]; then
     echo "[+] '$d' => $ip    $ipu"
     echo "$ip   $d   $ipu" >> hosts_filtered.txt
+  else
+    echo "[!] '$d' => [RESOLVE ERROR]"
   fi
 done < hosts.txt
-
