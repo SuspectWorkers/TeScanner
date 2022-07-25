2043087644539e4ccdacff0a31e3286dafe48801
SuspectWorkers <99353517+SuspectWorkers@users.noreply.github.com>
2022-07-26 02:34:33 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -8,10 +8,11 @@ while read d || [[ -n $d ]]; do
   ipus=$(curl -v --silent $d 2>&1 | grep -i x-cdn: | cut --delimiter="^" -f 1 |head -1)
   iput=$(curl -v --silent $d 2>&1 | grep -i x-cache: | cut --delimiter="^" -f 1 |head -1)
   ipum=$(curl -v --silent $d 2>&1 | grep -i x-id: | cut --delimiter="^" -f 1 |head -1)
+  redir=$(curl -v --silent $d 2>&1 | grep -i location: | cut --delimiter="^" -f 1 |head -1)
   ipas=$(curl -L -v --silent $d 2>&1 | grep "200 OK" | cut --delimiter=" " -f 2,3,4|head -1)
   if [ -n "$ip" ]; then
     echo "[+] '$d' => $ip    $ipu"
-    echo "$ip	$d	$ipu	$ipas  $ipua  $ipus  $iput  $ipum " >> hosts_filtered.txt
+    echo "$ip	$d	$ipu	$redir  $ipas  $ipua  $ipus  $iput  $ipum " >> hosts_filtered.txt
   else
     echo "[!] '$d' => [RESOLVE ERROR]"
   fi
@@ -20,7 +21,6 @@ tr -d '\15\32' < hosts_filtered.txt > output.txt
 rm hosts_filtered.txt
 
 read -p "Do you wish to save hosts onto your main storage (y / n)? " yn
-    if [[ "$yn" =~ 'y' ]]; then continue; fi
     if [[ "$yn" =~ 'n' ]]; then exit; fi
 
 cp output.txt /sdcard/output.txt
